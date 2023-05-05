3d48c53f2de6f9cce06d2d093753ea4448506eef
TheLordZedd <pcgpod@yahoo.com>
2023-05-04 20:52:51 -0500

    Added data access for the Book service layer

---
+++ src/main/java/com/tlz/librarysystem/service/BookService.java
@@ -0,0 +1,36 @@
+package com.tlz.librarysystem.service;
+
+import com.tlz.librarysystem.entity.Book;
+import com.tlz.librarysystem.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BookService {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    public List<Book> findAllBooks() {
+        return bookRepository.findAll();
+    }
+
+    public Book findBookById(Long id) {
+        Book book = bookRepository.findById(id).orElseThrow(() -> new RuntimeException("Book not found"));
+        return book;
+    }
+
+    public void createBook(Book book) {
+        bookRepository.save(book);
+    }
+
+    public void deleteBook(Long id) {
+        Book book = bookRepository.findById(id).orElseThrow(() -> new RuntimeException("Book not found"));
+        bookRepository.deleteById(book.getId());
+    }
+
+
+
+}
