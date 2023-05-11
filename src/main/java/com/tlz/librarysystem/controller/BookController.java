1247cf0c45b9ceb986c41fdf13c53aab5ac0e3e4
TheLordZedd <pcgpod@yahoo.com>
2023-05-10 23:43:02 -0500

    Added HTML (thymeleaf, bootstrap) for the list view and UI. Added toggle-able nav bar for mobile/smaller screens.

---
+++ src/main/java/com/tlz/librarysystem/controller/BookController.java
@@ -0,0 +1,24 @@
+package com.tlz.librarysystem.controller;
+
+import com.tlz.librarysystem.entity.Book;
+import com.tlz.librarysystem.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class BookController {
+
+    @Autowired
+    private BookService bookService;
+
+    @GetMapping("/books")
+    public String findAllBooks(Model model) {
+        List<Book> books = bookService.findAllBooks();
+        model.addAttribute("books", books);
+        return "books";
+    }
+}
